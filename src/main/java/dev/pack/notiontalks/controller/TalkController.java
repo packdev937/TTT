9b76a6cd90e27188ade5e935bd22070d5999c149
gundorit <packdev937@gmail.com>
2023-08-30 02:09:09 +0900

    feat : create findAll() and inject dependencies

--- src/main/java/dev/pack/notiontalks/controller/TalkController.java
+++ src/main/java/dev/pack/notiontalks/controller/TalkController.java
@@ -1,5 +1,11 @@
 package dev.pack.notiontalks.controller;
 
+import dev.pack.notiontalks.model.Talk;
+import dev.pack.notiontalks.notion.NotionClient;
+import dev.pack.notiontalks.notion.config.NotionConfigProperties;
+import dev.pack.notiontalks.notion.model.Page;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/talks")
 public class TalkController {
 
+    private final NotionClient notionClient;
+    private final NotionConfigProperties notionConfigProperties;
+
+    public TalkController(NotionClient notionClient,
+        NotionConfigProperties notionConfigProperties) {
+        this.notionClient = notionClient;
+        this.notionConfigProperties = notionConfigProperties;
+    }
+
+    @GetMapping
+    public List<Talk> findAll() {
+        List<Page> pages = notionClient.databaseService.query(notionConfigProperties.databaseId());
+        return null;
+    }
 }
