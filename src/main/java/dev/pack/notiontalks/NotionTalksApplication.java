e2e266f2877af13e1f91f3b6d29c8587322e1cab
gundorit <packdev937@gmail.com>
2023-08-30 01:09:56 +0900

    feat : add @EnableConfigurationProperties

--- src/main/java/dev/pack/notiontalks/NotionTalksApplication.java
+++ src/main/java/dev/pack/notiontalks/NotionTalksApplication.java
@@ -1,9 +1,12 @@
 package dev.pack.notiontalks;
 
+import dev.pack.notiontalks.config.NotionConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(NotionConfigProperties.class)
 public class NotionTalksApplication {
 
     public static void main(String[] args) {
