3a1da1485db8fa6db02c81c940d9ae38d92b1d01
gundorit <packdev937@gmail.com>
2023-08-30 02:01:19 +0900

    feat : inject DatabaseService.java

--- src/main/java/dev/pack/notiontalks/notion/NotionClient.java
+++ src/main/java/dev/pack/notiontalks/notion/NotionClient.java
@@ -1,5 +1,15 @@
 package dev.pack.notiontalks.notion;
 
+import dev.pack.notiontalks.notion.service.DatabaseService;
+
 public class NotionClient {
+
+    // Why public?
+    public final DatabaseService databaseService;
+
+    public NotionClient(DatabaseService databaseService) {
+        this.databaseService = databaseService;
+    }
+
 
 }
