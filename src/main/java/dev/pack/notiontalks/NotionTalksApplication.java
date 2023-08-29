52d0145e1be609c582bd3e534a702e0122e7cd09
gundorit <packdev937@gmail.com>
2023-08-30 01:01:46 +0900

    feat : Initial Setup

---
+++ src/main/java/dev/pack/notiontalks/NotionTalksApplication.java
@@ -0,0 +1,13 @@
+package dev.pack.notiontalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionTalksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotionTalksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pack/notiontalks/NotionTalksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pack.notiontalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionTalksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
