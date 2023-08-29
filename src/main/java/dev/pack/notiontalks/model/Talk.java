3a3e830d02fa2f15d8f6e6bf599e8f66628e9687
gundorit <packdev937@gmail.com>
2023-08-30 02:56:58 +0900

    fix : change format

--- src/main/java/dev/pack/notiontalks/model/Talk.java
+++ src/main/java/dev/pack/notiontalks/model/Talk.java
@@ -2,7 +2,6 @@ package dev.pack.notiontalks.model;
 
 import java.time.LocalDateTime;
 
-public record Talk(String id, String title, LocalDateTime startDate, LocalDateTime endDate,
-                   String url, String recording) {
+public record Talk (String id, String title, LocalDateTime startDate, LocalDateTime endDate, String url, String recording){
 
 }
