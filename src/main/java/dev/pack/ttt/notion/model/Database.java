de485afc335036d4197611ff17b23e1ce77cd610
gundorit <packdev937@gmail.com>
2023-09-04 20:45:32 +0900

    refactor : getting rid of Getter, Setter, toString and adding @Data, @ToString

--- src/main/java/dev/pack/ttt/notion/model/Database.java
+++ src/main/java/dev/pack/ttt/notion/model/Database.java
@@ -3,7 +3,11 @@ package dev.pack.ttt.notion.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class Database {
 
     private String object;
@@ -13,46 +17,4 @@ public class Database {
     private Boolean nextCursor;
     @JsonProperty("has_more")
     private Boolean hasMore;
-
-    public String getObject() {
-        return object;
-    }
-
-    public List<Page> getPages() {
-        return pages;
-    }
-
-    public Boolean getNextCursor() {
-        return nextCursor;
-    }
-
-    public Boolean getHasMore() {
-        return hasMore;
-    }
-
-    public void setObject(String object) {
-        this.object = object;
-    }
-
-    public void setPages(List<Page> pages) {
-        this.pages = pages;
-    }
-
-    public void setNextCursor(Boolean nextCursor) {
-        this.nextCursor = nextCursor;
-    }
-
-    public void setHasMore(Boolean hasMore) {
-        this.hasMore = hasMore;
-    }
-
-    @Override
-    public String toString() {
-        return "Database{" +
-            "object='" + object + '\'' +
-            ", pages=" + pages +
-            ", nextCursor=" + nextCursor +
-            ", hasMore=" + hasMore +
-            '}';
-    }
 }
--- src/main/java/dev/pack/ttt/notion/model/Page.java
+++ src/main/java/dev/pack/ttt/notion/model/Page.java
@@ -3,7 +3,11 @@ package dev.pack.ttt.notion.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class Page {
 
     private String object;
@@ -18,81 +22,4 @@ public class Page {
     // properties are consist of dynamic information
     private JsonNode properties;
     private JsonNode parent;
-
-    public JsonNode getParent() {
-        return parent;
-    }
-
-    public void setParent(JsonNode parent) {
-        this.parent = parent;
-    }
-
-    public String getObject() {
-        return object;
-    }
-
-    public void setObject(String object) {
-        this.object = object;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(LocalDateTime createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public LocalDateTime getLastEditedTime() {
-        return lastEditedTime;
-    }
-
-    public void setLastEditedTime(LocalDateTime lastEditedTime) {
-        this.lastEditedTime = lastEditedTime;
-    }
-
-    public Boolean getArchive() {
-        return archive;
-    }
-
-    public void setArchive(Boolean archive) {
-        this.archive = archive;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public JsonNode getProperties() {
-        return properties;
-    }
-
-    public void setProperties(JsonNode properties) {
-        this.properties = properties;
-    }
-
-    @Override
-    public String toString() {
-        return "Page{" +
-            "object='" + object + '\'' +
-            ", id='" + id + '\'' +
-            ", createdTime=" + createdTime +
-            ", last_edited_time=" + lastEditedTime +
-            ", archive=" + archive +
-            ", url='" + url + '\'' +
-            ", properties=" + properties +
-            '}';
-    }
 }
