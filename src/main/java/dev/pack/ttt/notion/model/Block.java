26d587d32d4669a67596c7758a6ce9ae1956b2fd
gundorit <packdev937@gmail.com>
2023-09-04 21:10:45 +0900

    feat : add default values

---
+++ src/main/java/dev/pack/ttt/notion/model/Block.java
@@ -0,0 +1,24 @@
+package dev.pack.ttt.notion.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Block {
+    private String object;
+    private String id;
+    @JsonProperty("created_time")
+    private LocalDateTime createdTime;
+    @JsonProperty("last_edited_time")
+    private LocalDateTime lastEditedTime;
+    private Boolean archive;
+    private String url;
+
+    // properties are consist of dynamic information
+    private JsonNode properties;
+    private JsonNode parent;
+}
