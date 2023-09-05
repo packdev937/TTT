e8c338b568032f6e8d072720bbbbe5d056187a9f
gundorit <packdev937@gmail.com>
2023-09-06 02:06:02 +0900

    feat : add Getter to Content.java

--- src/main/java/dev/pack/ttt/model/Content.java
+++ src/main/java/dev/pack/ttt/model/Content.java
@@ -5,4 +5,33 @@ import java.time.LocalDate;
 public record Content(String databaseId, String pageId, String title, LocalDate createdDate,
                       String category, Status status) {
 
+    @Override
+    public String databaseId() {
+        return databaseId;
+    }
+
+    @Override
+    public String pageId() {
+        return pageId;
+    }
+
+    @Override
+    public String title() {
+        return title;
+    }
+
+    @Override
+    public LocalDate createdDate() {
+        return createdDate;
+    }
+
+    @Override
+    public String category() {
+        return category;
+    }
+
+    @Override
+    public Status status() {
+        return status;
+    }
 }
