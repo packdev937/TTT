0b247f698c5ca710d693c1e2746e1139d4735061
gundorit <packdev937@gmail.com>
2023-09-06 15:30:07 +0900

    feat : 응답 Json Body의 기본 필드 추가

--- src/main/java/dev/pack/ttt/notion/model/Block.java
+++ src/main/java/dev/pack/ttt/notion/model/Block.java
@@ -1,22 +1,89 @@
 package dev.pack.ttt.notion.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
-import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.List;
 
 @Data
 @ToString
 public class Block {
-    private String object;
-    private String id;
-    @JsonProperty("created_time")
-    private LocalDateTime createdTime;
-    @JsonProperty("last_edited_time")
-    private LocalDateTime lastEditedTime;
-    private Boolean archive;
-    private String url;
+
+    private String type;
+
+    @JsonProperty("heading_1")
+    private RichTextWrapper heading1;
+
+    @JsonProperty("heading_2")
+    private RichTextWrapper heading2;
+
+    @JsonProperty("heading_3")
+    private RichTextWrapper heading3;
+
+    @JsonProperty("bulleted_list_item")
+    private RichTextWrapper bulletedListItem;
+
+    @JsonProperty("to_do")
+    private RichTextWrapper toDo;
+
+    @JsonProperty("numbered_list_item")
+    private RichTextWrapper numberedListItem;
+
+    @JsonProperty("paragraph")
+    private RichTextWrapper paragraph;
+
+    @JsonProperty("toggle")
+    private RichTextWrapper toggle;
+
+    @JsonProperty("quote")
+    private RichTextWrapper quote;
+
+    @JsonProperty("callout")
+    private RichTextWrapper callout;
+
+    @JsonProperty("code")
+    private CodeWrapper code;
+
+    @Data
+    @ToString
+    public static class RichTextWrapper {
+
+        @JsonProperty("rich_text")
+        private List<RichText> richText;
+    }
+
+    @Data
+    @ToString
+    public static class CodeWrapper {
+
+        @JsonProperty("rich_text")
+        private List<RichText> richText;
+
+        @JsonProperty("language")
+        private String language;
+    }
+
+    @Data
+    @ToString
+    public static class RichText {
+
+        private String type;
+        private Text text;
+        private Annotations annotations;
+        @JsonProperty("plain_text")
+        private String plainText;
+        private String href;
+    }
+
+    @Data
+    @ToString
+    public static class Text {
+
+        private String content;
+        private Object link;
+    }
 
     // properties are consist of dynamic information
     private JsonNode properties;
