afb48252c0ca26f9e1efa46d000d24831845c196
gundorit <packdev937@gmail.com>
2023-09-06 16:26:20 +0900

    feat : @EnabledConfigurationProperties에 TistoryConfigProperties 추가

--- src/main/java/dev/pack/ttt/NotionTalksApplication.java
+++ src/main/java/dev/pack/ttt/NotionTalksApplication.java
@@ -1,12 +1,13 @@
 package dev.pack.ttt;
 
 import dev.pack.ttt.notion.config.NotionConfigProperties;
+import dev.pack.ttt.tistory.config.TistoryConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(NotionConfigProperties.class)
+@EnableConfigurationProperties({NotionConfigProperties.class, TistoryConfigProperties.class})
 public class NotionTalksApplication {
 
     public static void main(String[] args) {
