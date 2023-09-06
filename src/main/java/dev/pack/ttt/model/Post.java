c5344e15bbc84a91cfb4183de20180126261f71c
gundorit <packdev937@gmail.com>
2023-09-06 15:37:11 +0900

    feat : add @ToString and @Builder to Post.java

--- src/main/java/dev/pack/ttt/model/Post.java
+++ src/main/java/dev/pack/ttt/model/Post.java
@@ -1,10 +1,18 @@
 package dev.pack.ttt.model;
 
+import dev.pack.ttt.notion.model.Block;
+import java.util.List;
+import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
+@Builder
 public class Post {
 
+    // Tistory에 Post할 때 필요한 정보들로 대체
     private Content content;
-    private String body;
+    private List<Block> blocks;
+    private String html;
 }
