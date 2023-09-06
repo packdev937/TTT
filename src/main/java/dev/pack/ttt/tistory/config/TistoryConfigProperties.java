26999f1a39d100b7d610bfff012c86cfc0d00731
gundorit <packdev937@gmail.com>
2023-09-06 16:34:01 +0900

    feat : add property accessToken

--- src/main/java/dev/pack/ttt/tistory/config/TistoryConfigProperties.java
+++ src/main/java/dev/pack/ttt/tistory/config/TistoryConfigProperties.java
@@ -3,6 +3,6 @@ package dev.pack.ttt.tistory.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("tistory")
-public record TistoryConfigProperties(String apiUrl, String blogName) {
+public record TistoryConfigProperties(String apiUrl, String blogName, String accessToken) {
 
 }
