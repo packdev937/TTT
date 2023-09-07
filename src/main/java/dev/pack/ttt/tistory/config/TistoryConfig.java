2ad7d3c774e40d8dc6b3605d86b714fba1d9a17d
gundorit <packdev937@gmail.com>
2023-09-07 14:45:19 +0900

    fix : remove RestTemplate Bean to remove redunduncy

--- src/main/java/dev/pack/ttt/tistory/config/TistoryConfig.java
+++ src/main/java/dev/pack/ttt/tistory/config/TistoryConfig.java
@@ -1,14 +1,7 @@
 package dev.pack.ttt.tistory.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class TistoryConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
 }
