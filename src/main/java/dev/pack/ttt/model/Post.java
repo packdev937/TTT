c1f1437d6e550094f6ecc73e902f0aa6fa8f2a8a
gundorit <packdev937@gmail.com>
2023-09-07 14:44:30 +0900

    feat : add properties used in Tistory POST API

--- src/main/java/dev/pack/ttt/model/Post.java
+++ src/main/java/dev/pack/ttt/model/Post.java
@@ -1,6 +1,7 @@
 package dev.pack.ttt.model;
 
 import dev.pack.ttt.notion.model.Block;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +15,11 @@ public class Post {
     // Tistory에 Post할 때 필요한 정보들로 대체
     private Content content;
     private List<Block> blocks;
-    private String html;
+    private int visibility;
+    private int category;
+    private String slogan;
+    private String tag;
+    private LocalDate publishedDate;
+    private StringBuilder html;
+
 }
