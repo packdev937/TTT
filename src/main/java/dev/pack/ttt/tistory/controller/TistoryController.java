910c124dad1f9aedece3c3a8d179bd67b7595487
gundorit <packdev937@gmail.com>
2023-09-11 00:06:04 +0900

    feat : /upload 요청 처리

---
+++ src/main/java/dev/pack/ttt/tistory/controller/TistoryController.java
@@ -0,0 +1,36 @@
+package dev.pack.ttt.tistory.controller;
+
+import dev.pack.ttt.model.Post;
+import dev.pack.ttt.service.NotionService;
+import dev.pack.ttt.tistory.service.TistoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TistoryController {
+
+    private final TistoryService tistoryService;
+    private final NotionService notionService;
+
+    @GetMapping("/upload")
+    public String upload() {
+        String response = "";
+        List<Post> posts = notionService.convertBlockToPost(); // 여기서 return 되어서 넘어오는게 없음
+        // empty set
+        System.out.println(posts.get(0).getHtml());
+        for (Post post : posts) {
+            try {
+                response = tistoryService.post(post);
+            } catch (Exception e) {
+                log.info("{}", e);
+            }
+        }
+        return response;
+    }
+
+}
