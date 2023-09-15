a76e993eaae3df7345e172e9578870b5d7e9b4b5
gundorit <packdev937@gmail.com>
2023-09-15 09:22:05 +0900

    feat : patch() 작성

--- src/main/java/dev/pack/ttt/notion/service/DatabaseService.java
+++ src/main/java/dev/pack/ttt/notion/service/DatabaseService.java
@@ -5,7 +5,9 @@ import dev.pack.ttt.notion.model.Block;
 import dev.pack.ttt.notion.model.Content;
 import dev.pack.ttt.notion.model.Database;
 import dev.pack.ttt.notion.model.Page;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
@@ -58,18 +60,34 @@ public class DatabaseService {
         return db.getBody().getResults();
     }
 
-    public String patch(Content content) {
+    public String patch(String pageId) {
         String url =
-            notionConfigProperties.apiUrl() + "/v1/databases/" + content.pageId();
+            notionConfigProperties.apiUrl() + "/v1/pages/" + pageId;
+        log.info("Updating Notion page: {}", url);
 
-        ResponseEntity<String> response = restTemplate.exchange(
+        Map<String, Object> selectProperty = new HashMap<>();
+        selectProperty.put("name", "COMPLETE");
+
+        Map<String, Object> statusProperty = new HashMap<>();
+        statusProperty.put("select", selectProperty);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("Status", statusProperty);
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("properties", properties);
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody,
+            getDefaultHeaders());
+
+        ResponseEntity<String> db = restTemplate.exchange(
             url,
             HttpMethod.PATCH,
-            new HttpEntity<>(getDefaultHeaders()),
+            requestEntity,
             new ParameterizedTypeReference<String>() {
             }
         );
-        return url;
+        return db.getBody();
     }
 
     private HttpHeaders getDefaultHeaders() {
